098cd356b5e23817d8fe0977c725417698c09104
filippo savi <filssavi@gmail.com>
2024-09-29 16:43:23 +0200

    add error reporting for missing auth token

--- src/API/backend/middleware.ts
+++ src/API/backend/middleware.ts
@@ -19,13 +19,24 @@ export function authorizer() {
         let stripped_route = ctx.path.replace(endpoints_map.global_prefix, "");
         let [api_class, ...rest] = stripped_route.slice(1).split('/')
         let requested_endpoint = rest.join('/')
+        if(!endpoints_map.hasOwnProperty(api_class)){
+            ctx.status = 404;
+            ctx.body = "unknown endpoint";
+            return;
+        }
 
         for(let item in endpoints_map[api_class].endpoints){
             let found_endpoint = endpoints_map[api_class].endpoints[item].slice(1);
             if(found_endpoint === requested_endpoint){
-                let required_role = roles_hierarchy[rules[api_class][item]];
+                let rule = rules[api_class][item];
+                let required_role = roles_hierarchy[rule];
                 if(required_role===99){
                     await next();
+                    return;
+                }
+                if(!ctx.state.hasOwnProperty("user")){
+                    ctx.status = 403;
+                    ctx.body = "Authorization token missing";
                     return;
                 }
                 let user_role = roles_hierarchy[ctx.state.user.role];
